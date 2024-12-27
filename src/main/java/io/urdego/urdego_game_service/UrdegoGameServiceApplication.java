2b5186c5f012859b00feeab501e7c9e48dada882
ooni <gdbsrjsdn@gmail.com>
2024-12-27 15:42:32 +0900

    start project

---
+++ src/main/java/io/urdego/urdego_game_service/UrdegoGameServiceApplication.java
@@ -0,0 +1,13 @@
+package io.urdego.urdego_game_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrdegoGameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrdegoGameServiceApplication.class, args);
+    }
+
+}
