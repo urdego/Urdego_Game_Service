42b1766554c3e66258a48ed0ee1de2deda3ed29f
ooni <gdbsrjsdn@gmail.com>
2025-01-16 13:47:16 +0900

    Feature(#3) : 대기방 관련 dto 추가

--- src/main/java/io/urdego/urdego_game_service/api/room/dto/response/RoomCreateRes.java
+++ src/main/java/io/urdego/urdego_game_service/api/room/dto/response/RoomCreateRes.java
@@ -1,4 +1,16 @@
 package io.urdego.urdego_game_service.api.room.dto.response;
 
-public record RoomCreateRes() {
+import io.urdego.urdego_game_service.common.enums.Status;
+import io.urdego.urdego_game_service.domain.room.entity.Room;
+
+public record RoomCreateRes(
+        String roomId,
+        Status status
+) {
+    public static RoomCreateRes from(Room room) {
+        return new RoomCreateRes(
+                room.getRoomId(),
+                room.getStatus()
+        );
+    }
 }
