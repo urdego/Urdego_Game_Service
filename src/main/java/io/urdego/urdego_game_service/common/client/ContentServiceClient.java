66470d0e829354c94a038748f7ca1cfc0e6bfd12
ooni <gdbsrjsdn@gmail.com>
2025-01-21 18:51:56 +0900

    BugFix(#11) : feign service 로컬 말고 배포 버전에서 끌어다 쓰도록 설정

--- src/main/java/io/urdego/urdego_game_service/common/client/ContentServiceClient.java
+++ src/main/java/io/urdego/urdego_game_service/common/client/ContentServiceClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
-@FeignClient(name = "content-service")
+@FeignClient(name = "content-service", url = "${feign.client.config.content-service.url}")
 public interface ContentServiceClient {
     @GetMapping("/api/content-service/content")
     ContentRes getContent(@RequestParam Long contentId);
