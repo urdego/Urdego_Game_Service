6e3f12b9409181ecdc1b3c66e01f66d32b792aed
ooni <gdbsrjsdn@gmail.com>
2025-02-11 20:14:40 +0900

    Feature(#18) : userId 말고 nickname으로 응답

--- src/main/java/io/urdego/urdego_game_service/controller/room/dto/response/RoomCreateRes.java
+++ src/main/java/io/urdego/urdego_game_service/controller/room/dto/response/RoomCreateRes.java
@@ -1,9 +1,12 @@
 package io.urdego.urdego_game_service.controller.room.dto.response;
 
 import io.urdego.urdego_game_service.common.enums.Status;
+import io.urdego.urdego_game_service.controller.client.user.dto.UserRes;
 import io.urdego.urdego_game_service.domain.room.entity.Room;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public record RoomCreateRes(
         String roomId,
@@ -11,12 +14,19 @@ public record RoomCreateRes(
         Status status,
         List<String> currentPlayers
 ) {
-    public static RoomCreateRes from(Room room) {
+    public static RoomCreateRes from(Room room, List<UserRes> users) {
+        Map<String, String> userIdToNickname = users.stream()
+                .collect(Collectors.toMap(userRes -> String.valueOf(userRes.userId()), UserRes::nickname));
+
+        List<String> currentPlayers = room.getCurrentPlayers().stream()
+                .map(userIdToNickname::get)
+                .toList();
+
         return new RoomCreateRes(
                 room.getRoomId(),
                 room.getRoomName(),
                 room.getStatus(),
-                room.getCurrentPlayers()
+                currentPlayers
         );
     }
 }
--- src/main/java/io/urdego/urdego_game_service/controller/room/dto/response/RoomPlayersRes.java
+++ src/main/java/io/urdego/urdego_game_service/controller/room/dto/response/RoomPlayersRes.java
@@ -1,30 +1,44 @@
 package io.urdego.urdego_game_service.controller.room.dto.response;
 
 import io.urdego.urdego_game_service.common.enums.Status;
+import io.urdego.urdego_game_service.controller.client.user.dto.UserRes;
 import io.urdego.urdego_game_service.domain.room.entity.Room;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public record RoomPlayersRes(
         String roomId,
         Status status,
         List<String> currentPlayers,
-        String isHost,
+        String host,
         Map<String, Boolean> readyStatus,
         Boolean allReady
 ) {
-    public static RoomPlayersRes from(Room room) {
-        String hostId = room.getCurrentPlayers().isEmpty() ? null : room.getCurrentPlayers().get(0);
+    public static RoomPlayersRes from(Room room, List<UserRes> users) {
+        // userId -> nickname 매핑 메서드
+        Map<String, String> userIdToNickname = users.stream()
+                .collect(Collectors.toMap(userRes -> String.valueOf(userRes.userId()), UserRes::nickname));
+
+        List<String> currentPlayers = room.getCurrentPlayers().stream()
+                .map(userIdToNickname::get)
+                .toList();
+
+        String host = room.getCurrentPlayers().isEmpty() ? null : userIdToNickname.get(room.getCurrentPlayers().get(0));
+
+        Map<String, Boolean> readyStatus = room.getReadyStatus().entrySet().stream()
+                .collect(Collectors.toMap(entry -> userIdToNickname.get(entry.getKey()), Map.Entry::getValue));
+
         boolean allReady = room.getReadyStatus().size() == room.getCurrentPlayers().size()
                 && room.getReadyStatus().values().stream().allMatch(ready -> ready);
 
         return new RoomPlayersRes(
                 room.getRoomId(),
                 room.getStatus(),
-                room.getCurrentPlayers(),
-                hostId,
-                room.getReadyStatus(),
+                currentPlayers,
+                host,
+                readyStatus,
                 allReady
         );
     }
