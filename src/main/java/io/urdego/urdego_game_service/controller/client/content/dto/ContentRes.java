fdf346d6b61b2f77b69abaed71c14a91253d3a8e
GeonWoo <gdbsrjsdn@gmail.com>
2025-02-24 17:18:40 +0900

    HotFix: Content 부분 url로 반환하도록 수정

--- src/main/java/io/urdego/urdego_game_service/controller/client/content/dto/ContentRes.java
+++ src/main/java/io/urdego/urdego_game_service/controller/client/content/dto/ContentRes.java
@@ -1,11 +1,15 @@
 package io.urdego.urdego_game_service.controller.client.content.dto;
 
+import java.time.LocalDateTime;
+
 public record ContentRes(
         Long contentId,
+        String url,
         String contentName,
         String address,
         double latitude,
         double longitude,
-        String hint
+        String hint,
+        LocalDateTime createdDateTime
 ) {
 }
